3ea72023d5430fd5ad0345391029a2ae78a99050
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-12 19:14:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
+
+
+
+/* MILESTONE 1
+Al caricamento dell'applicazione, recuperiamo la lista degli attori e delle attrici dalle API e stampiamoli in console.
+
+MILESTONE 2
+Prepariamo una card per ciascun attore/attrice, mostrandone le seguenti informazioni:
+nome 
+anno nascita
+nazionalità
+biografia
+immagine
+riconoscimenti
+
+MILESTONE 3
+Mostriamo in pagina una card per ciascun attore, con grafica a piacimento! */
